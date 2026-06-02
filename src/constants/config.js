bf1298db8bfe41c869c9acfc9eed36eff19e614a
Daniel <53100709+PLT-piwi@users.noreply.github.com>
2026-06-02 18:05:55 -0400

    Add donation options to config

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,2 +1,19 @@
 export const UNASSIGNED_ID = "unassigned";
 export const STORAGE_KEY = "malla-idi-v3";
+export const DONATION_OPTIONS = [
+  {
+    label: "Cafe",
+    amount: "$1.000 CLP",
+    url: "https://ko-fi.com/ds752607",
+  },
+  {
+    label: "Apoyo",
+    amount: "$3.000 CLP",
+    url: "https://ko-fi.com/ds752607",
+  },
+  {
+    label: "Mega apoyo",
+    amount: "$5.000 CLP",
+    url: "https://ko-fi.com/ds752607",
+  },
+];
